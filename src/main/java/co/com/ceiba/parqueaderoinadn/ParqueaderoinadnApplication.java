9c6647979788aba5bc65b2ec9a64db2ae21dff41
Juan Gabriel González Hortúa - Ceiba Software <gabriel.gonzalez@ceiba.com.co>
2018-08-06 14:17:32 -0500

    first commit

---
+++ src/main/java/co/com/ceiba/parqueaderoinadn/ParqueaderoinadnApplication.java
@@ -0,0 +1,12 @@
+package co.com.ceiba.parqueaderoinadn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoinadnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoinadnApplication.class, args);
+	}
+}
---
+++ src/test/java/co/com/ceiba/parqueaderoinadn/ParqueaderoinadnApplicationTests.java
@@ -0,0 +1,16 @@
+package co.com.ceiba.parqueaderoinadn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParqueaderoinadnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
